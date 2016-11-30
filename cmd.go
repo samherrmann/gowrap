db0d140743eeda6fc9f7bbce40213abc0cf780aa
Sam Herrmann <webocular@gmail.com>
2016-11-30 02:18:50 -0500

    Fix doc typos

--- cmd.go
+++ cmd.go
@@ -10,21 +10,21 @@ func cmd(name string, args ...string) *Cmd {
 	return c
 }
 
-// Cmd extends exec Cmd
+// Cmd extends exec.Cmd
 // from Go's standard library.
 type Cmd struct {
 	*exec.Cmd
 }
 
 // Run calls Go's cmd.Run() and panics
-// is an error occurs.
+// if an error occurs.
 func (c *Cmd) Run() {
 	err := c.Cmd.Run()
 	panicIf(err)
 }
 
 // Output calls Go's cmd.Output() and panics
-// is an error occurs.
+// if an error occurs.
 func (c *Cmd) Output() []byte {
 	out, err := c.Cmd.Output()
 	panicIf(err)
