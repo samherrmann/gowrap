ca4b43debc138f4fd650b2c877fd35b9118037c5
Sam Herrmann <webocular@gmail.com>
2016-11-30 02:13:26 -0500

    Move readOrSaveConfig func to utils.go file

--- main.go
+++ main.go
@@ -1,10 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"gowrap/config"
-	"os"
-)
+import "fmt"
 
 var (
 	// name of the application
@@ -24,18 +20,5 @@ func main() {
 		fmt.Println("Building " + buildName(name, version, goos, goarch) + "...")
 		goGenerate()
 		goBuild(name, version, goos, goarch)
-	}
-}
-
-func readOrSaveConfig(c *Config) {
-	readErr, saveErr := config.ReadOrSave("gowrap.json", c)
-	if readErr != nil && saveErr != nil {
-		panicIf(saveErr)
-	}
-	if readErr != nil && saveErr == nil {
-		fmt.Println("No 'gowrap.json' file found. " +
-			"A sample file was created in the current directory. " +
-			"Edit the file as required and re-run gowrap.")
-		os.Exit(0)
 	}
 }
--- utils.go
+++ utils.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"gowrap/config"
 	"os"
 	"path/filepath"
 )
@@ -23,4 +25,20 @@ func currentFolderName() string {
 // ex: myapp-v1.0.0-linux-amd64
 func buildName(name string, version string, os string, arch string) string {
 	return name + "-" + version + "-" + os + "-" + arch
+}
+
+// readOrSave attempts to read the configuration file. If it
+// cannot find the file, it will save a sample configuration
+// file instead.
+func readOrSaveConfig(c *Config) {
+	readErr, saveErr := config.ReadOrSave("gowrap.json", c)
+	if readErr != nil && saveErr != nil {
+		panicIf(saveErr)
+	}
+	if readErr != nil && saveErr == nil {
+		fmt.Println("No 'gowrap.json' file found. " +
+			"A sample file was created in the current directory. " +
+			"Edit the file as required and re-run gowrap.")
+		os.Exit(0)
+	}
 }
