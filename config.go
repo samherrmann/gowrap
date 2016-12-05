fa8079791cb3f4af91d41316b17699c220ebbff3
Sam Herrmann <webocular@gmail.com>
2016-12-05 14:24:01 -0500

    Move readOrSave func from utils.go to config.go

--- config.go
+++ config.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"errors"
 	"strings"
 
 	"github.com/samherrmann/gowrap/gotools"
+	"github.com/samherrmann/gowrap/jsonfile"
 )
 
 func NewConfig() *Config {
@@ -30,4 +32,24 @@ type Target string
 func (t *Target) Parse() (goos string, goarch string) {
 	s := strings.Split(string(*t), "-")
 	return s[0], s[1]
+}
+
+// readOrSave attempts to read the configuration file.
+// If it cannot find the file, it will save a sample
+// configuration file instead.
+func readOrSaveConfig() (*Config, error) {
+	c := NewConfig()
+	filePath := "gowrap.json"
+
+	if err := jsonfile.Read(filePath, c); err == nil {
+		return c, nil
+	}
+
+	c = NewConfig()
+	err := jsonfile.Write(filePath, c)
+	panicIf(err)
+
+	return nil, errors.New("No 'gowrap.json' file found. " +
+		"A sample file was created in the current directory. " +
+		"Edit the file as required and re-run gowrap.")
 }
--- utils.go
+++ utils.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"errors"
 	"os"
 	"path/filepath"
-
-	"github.com/samherrmann/gowrap/jsonfile"
 )
 
 func panicIf(err error) {
@@ -20,24 +17,4 @@ func currentFolderName() string {
 	dir, err := os.Getwd()
 	panicIf(err)
 	return filepath.Base(dir)
-}
-
-// readOrSave attempts to read the configuration file.
-// If it cannot find the file, it will save a sample
-// configuration file instead.
-func readOrSaveConfig() (*Config, error) {
-	c := NewConfig()
-	filePath := "gowrap.json"
-
-	if err := jsonfile.Read(filePath, c); err == nil {
-		return c, nil
-	}
-
-	c = NewConfig()
-	err := jsonfile.Write(filePath, c)
-	panicIf(err)
-
-	return nil, errors.New("No 'gowrap.json' file found. " +
-		"A sample file was created in the current directory. " +
-		"Edit the file as required and re-run gowrap.")
 }
